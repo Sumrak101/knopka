805ba9a29ccb274a6da57c7fe73b047b957b7a6e
Александр <ms.popova444@mail.ru>
2022-09-28 13:56:58 +0300

    мое первое приложение

---
+++ app/src/main/java/com/example/knopka/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.knopka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView helloText;
+    private ImageButton startButton;
+    private ImageButton android;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        helloText = (TextView) findViewById(R.id.helloText);
+    }
+
+    public void onStartButtonClick(View view) {
+        startButton.setImageResource(R.drawable.android);
+        helloText.setVisibility(View.VISIBLE);
+        startButton.setClickable(false);
+    }
+}
